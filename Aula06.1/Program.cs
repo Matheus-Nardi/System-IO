9b8d6277597d23366b2ffdb0d929fc8ff47597dc
Matheus <matheuznardi@gmail.com>
2023-09-11 15:39:58 -0300

    Aulas novas

---
+++ Aula06.1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Text;
+
+namespace Aula06._1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using(FileStream fluxo = File.Create("arquivo6.1.txt"))
+        using (StreamWriter escritor = new StreamWriter(fluxo , Encoding.UTF8) )
+        {
+             byte[] dados = Encoding.UTF8.GetBytes("Isso é um array de dados");
+
+
+             for(int i= 0 ; i <dados.Length; i++)
+             {
+                fluxo.WriteByte(dados[i]);
+             }
+
+ 
+ 
+        }    
+     }       
+}
---
+++ Aula07/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using System.Text;
+namespace Aula07;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        FileStream stream  = new FileStream("Aula05\arquivo5.txt ", FileMode.Open , FileAccess.Read); //Criação do Stream , para abrir e ler o arquivo 5 
+
+
+        StreamReader leitor = new StreamReader(stream , Encoding.UTF8) ;
+
+        string line ; // var sentinela  
+      //  while( (line = leitor.ReadLine()) != null)
+       // {
+       //     Console.WriteLine(line);
+      //  }
+
+
+      //Outra maneira utilizando leitor.Peek
+
+      while(leitor.Peek() > 0)
+      {
+        line = leitor.ReadLine();
+        Console.WriteLine(line);
+      }
+
+
+    }
+}
