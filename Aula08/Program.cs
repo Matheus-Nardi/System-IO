973de3ad64dbc6c0830098a74a2f09477b9c1b07
Matheus <matheuznardi@gmail.com>
2023-09-12 14:50:24 -0300

    Nova Aula

---
+++ Aula08/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using System.Text;
+
+namespace Aula08;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string arquivo = @"C:\Users\55639\Área de Trabalho\Mira.txt";
+
+        using(FileStream streamLido = new FileStream (arquivo , FileMode.Open , FileAccess.Read))
+        using(FileStream streamEscrito = new FileStream("Valorant.txt" , FileMode.Create , FileAccess.Write))
+        {
+                int  bytesLidos = 0 ;
+                byte[] buffer = new byte[4096];
+
+                while((bytesLidos = streamLido.Read(buffer , 0 , buffer.Length)) > 0 )
+                {
+                    streamEscrito.Write(buffer , 0 , bytesLidos);
+
+                    Console.WriteLine("Mais 4kb foram escritos");
+                }
+        }
+       
+    }
+}
